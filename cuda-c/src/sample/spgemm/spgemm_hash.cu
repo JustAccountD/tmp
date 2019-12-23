fae9bc4e030c1196fae072bdaf81f3aaeb4a6c6c
DmiitriyJarosh <dimajarosh@gmail.com>
2019-12-23 12:22:20 +0300

    algo loop added

--- cuda-c/src/sample/spgemm/spgemm_hash.cu
+++ cuda-c/src/sample/spgemm/spgemm_hash.cu
@@ -132,7 +132,7 @@ void spgemm_csr(sfCSR *a, sfCSR *b, sfCSR *c, int grSize, unsigned short int * g
 #endif
             spgemm_kernel_hash(a, b, c, grSize, grBody, grTail);
 #ifdef FLOAT
-            printf("Succes mult!!\n");
+            printf("Success mult!!\n");
             checkCudaErrors(cudaMalloc((void **)&(b->d_col), sizeof(int) * (a->nnz + c->nnz)));
             checkCudaErrors(cudaMalloc((void **)&(b->d_val), sizeof(real) * (a->nnz + c->nnz)));
             sumSparse<<<1, 1>>>(a->M, a->d_rpt, a->d_val, a->d_col, c->d_rpt, c->d_val, c->d_col, b->d_rpt, b->d_val, b->d_col);
@@ -145,9 +145,9 @@ void spgemm_csr(sfCSR *a, sfCSR *b, sfCSR *c, int grSize, unsigned short int * g
         cudaThreadSynchronize();
         cudaEventElapsedTime(&msec, event[0], event[1]);
 
-        if (i > 0) {
+//        if (i > 0) {
             ave_msec += msec;
-        }
+//        }
     }
     //ave_msec /= SPGEMM_TRI_NUM - 1;
   
