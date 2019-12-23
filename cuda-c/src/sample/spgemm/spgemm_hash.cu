215a35f03841c566269c695c97d7842b4afbd28c
DmiitriyJarosh <dimajarosh@gmail.com>
2019-12-23 14:43:55 +0300

    algo loop added

--- cuda-c/src/sample/spgemm/spgemm_hash.cu
+++ cuda-c/src/sample/spgemm/spgemm_hash.cu
@@ -144,6 +144,7 @@ void spgemm_csr(sfCSR *a, sfCSR *b, sfCSR *c, int grSize, unsigned short int * g
             cudaFree(b->d_val);
             checkCudaErrors(cudaMalloc((void **)&(b->d_col), sizeof(int) * (a->nnz + c->nnz)));
             checkCudaErrors(cudaMalloc((void **)&(b->d_val), sizeof(real) * (a->nnz + c->nnz)));
+            printf("Not null in C: %d", c->nnz);
             sumSparse<<<1, 1>>>(a->M, a->d_rpt, a->d_val, a->d_col, c->d_rpt, c->d_val, c->d_col, b->d_rpt, b->d_val, b->d_col);
             csr_copy(b, a);
             csr_copy(a, b);
