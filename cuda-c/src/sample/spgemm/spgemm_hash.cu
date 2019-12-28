19236f42c231663d3314b0f70cb2fca90f61f715
DmiitriyJarosh <dimajarosh@gmail.com>
2019-12-28 18:56:09 +0300

    Reader added

--- cuda-c/src/sample/spgemm/spgemm_hash.cu
+++ cuda-c/src/sample/spgemm/spgemm_hash.cu
@@ -335,7 +335,7 @@ void load_graph(const std::string & graph_filename, sfCSR * matrix) {
         val_coo[i] = bool_vector;
     }
 
-
+    printf("MATRIX loader: dimension: %d, nnz: %d\n", matrix->M, matrix->nnz);
     /* Count the number of non-zero in each row */
     int num = matrix->N;
     int * nnz_num = (int *)malloc(sizeof(int) * matrix->M);
