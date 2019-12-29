61ad8214a24f01f0682b40b4220c5ea0ae56cc1b
DmiitriyJarosh <dimajarosh@gmail.com>
2019-12-29 22:40:39 +0300

    Reader added

--- cuda-c/src/sample/spgemm/spgemm_hash.cu
+++ cuda-c/src/sample/spgemm/spgemm_hash.cu
@@ -188,7 +188,7 @@ void spgemm_csr(sfCSR *a, sfCSR *b, sfCSR *c, int grSize, unsigned short int * g
             }
             cudaThreadSynchronize();
         }
-        printf("Average 'in sum' time: %d\n", ave_msec_sum / u);
+        printf("Average 'in sum' time: %f\n", ave_msec_sum / u);
 #endif
         cudaEventRecord(event[1], 0);
         cudaThreadSynchronize();
