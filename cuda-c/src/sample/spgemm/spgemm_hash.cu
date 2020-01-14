113d11196ce19c81d2c440dbddf2a35d3fd1ed68
DmiitriyJarosh <dimajarosh@gmail.com>
2020-01-14 09:27:47 +0300

    Copy off

--- cuda-c/src/sample/spgemm/spgemm_hash.cu
+++ cuda-c/src/sample/spgemm/spgemm_hash.cu
@@ -136,7 +136,7 @@ void spgemm_csr(sfCSR *a, sfCSR *b, sfCSR *c, int grSize, unsigned short int * g
 
     /* Execution of SpGEMM on Device */
     ave_msec = 0;
-    unsigned int ave_msec_sum = 0;
+    unsigned int ave_msec_sum = 0, ave_msec_copy = 0;
     for (i = 0; i < SPGEMM_TRI_NUM; i++) {
         if (i > 0) {
             release_csr(*c);
@@ -167,22 +167,27 @@ void spgemm_csr(sfCSR *a, sfCSR *b, sfCSR *c, int grSize, unsigned short int * g
             checkCudaErrors(cudaMalloc((void **)&(b->d_col), sizeof(int) * (a->nnz + c->nnz)));
             checkCudaErrors(cudaMalloc((void **)&(b->d_val), sizeof(real) * (a->nnz + c->nnz)));
             printf("Ready for sum!!\n");
-
+            high_resolution_clock::time_point begin_sum_time = high_resolution_clock::now();
             sumSparse<<<1, 1>>>(a->M, a->d_rpt, a->d_val, a->d_col, c->d_rpt, c->d_val, c->d_col, b->d_rpt, b->d_val, b->d_col);
+            cudaThreadSynchronize();
+            high_resolution_clock::time_point end_sum_time = high_resolution_clock::now();
             printf("Success sum!!\n");
+            milliseconds elapsed_secs = duration_cast<milliseconds>(end_sum_time - begin_sum_time);
+            ave_msec_sum += static_cast<unsigned int>(elapsed_secs.count());
 
             printf("Ready for copy!!\n");
-            high_resolution_clock::time_point begin_sum_time = high_resolution_clock::now();
+
+            high_resolution_clock::time_point begin_copy_time = high_resolution_clock::now();
             cudaMemcpyFromSymbol(&nnzS, nnzSum, sizeof(int), 0, cudaMemcpyDeviceToHost);
             b->nnz = nnzS;
-
             sfCSR * tmp = b;
             b = a;
             a = tmp;
-            high_resolution_clock::time_point end_sum_time = high_resolution_clock::now();
+            high_resolution_clock::time_point end_copy_time = high_resolution_clock::now();
+
             printf("Success copy!!\n");
-            milliseconds elapsed_secs = duration_cast<milliseconds>(end_sum_time - begin_sum_time);
-            ave_msec_sum += static_cast<unsigned int>(elapsed_secs.count());
+            milliseconds elapsed_secs_c = duration_cast<milliseconds>(end_copy_time - begin_copy_time);
+            ave_msec_copy += static_cast<unsigned int>(elapsed_secs_c.count());
 
             //printf("NNZ of sum: %d RPT last of sum: %d\n", b->nnz, b->rpt[4]);
             cudaError_t result = cudaGetLastError();
@@ -197,7 +202,8 @@ void spgemm_csr(sfCSR *a, sfCSR *b, sfCSR *c, int grSize, unsigned short int * g
             }
             cudaThreadSynchronize();
         }
-        printf("Average 'in copy' time: %d %d %f\n", ave_msec_sum, u, ave_msec_sum / (double)u);
+        printf("Average 'in sum' time: %d %d %f\n", ave_msec_sum, u, ave_msec_sum / (double)u);
+        printf("Average 'in copy' time: %d %d %f\n", ave_msec_copy, u, ave_msec_copy / (double)u);
 #endif
         cudaEventRecord(event[1], 0);
         cudaThreadSynchronize();
