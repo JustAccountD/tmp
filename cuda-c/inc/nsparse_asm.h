d6c528863ccf7986262aeade018e1b9d07df9936
DmiitriyJarosh <dimajarosh@gmail.com>
2020-01-23 12:46:16 +0300

    Matrix printf

--- cuda-c/inc/nsparse_asm.h
+++ cuda-c/inc/nsparse_asm.h
@@ -107,9 +107,9 @@ __device__ unsigned short atomicAddShort(unsigned short * address, unsigned shor
     unsigned int long_val = ((size_t)address & 2) ? ((unsigned int)val << 16) : (unsigned short)val;
     unsigned short beforeOr = *address;
     unsigned int long_old = atomicOr(base_address, long_val);
-    if (val != 0) {
-        //printf("SUM: %x | %x = %x (before or: %x) Address: %p Base address: %p Longval: %x\n", checktmp, val, *address, beforeOr, address, base_address, long_val);
-    }
+//    if (val != 0) {
+        printf("SUM: %x | %x = %x (before or: %x) Address: %p Base address: %p Longval: %x\n", checktmp, val, *address, beforeOr, address, base_address, long_val);
+//    }
     if ((size_t)address & 2) {
         return (unsigned short)(long_old >> 16);
     } else {
