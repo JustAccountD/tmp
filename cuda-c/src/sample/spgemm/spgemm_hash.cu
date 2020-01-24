7a36f9e0a64ca7ff3075ef65e60c087b51ad1e1a
DmiitriyJarosh <dimajarosh@gmail.com>
2020-01-24 12:04:14 +0300

    Matrix printf

--- cuda-c/src/sample/spgemm/spgemm_hash.cu
+++ cuda-c/src/sample/spgemm/spgemm_hash.cu
@@ -513,11 +513,11 @@ void load_graph(const std::string & graph_filename, sfCSR * matrix) {
         tempVec.push_back({{edge.second.first, edge.second.second}, bool_vector});
     }
 
-//    sort(tempVec.begin(), tempVec.end(),
-//    [](const pair<pair<int, int>, unsigned short> & a, const pair<pair<int, int>, unsigned short> & b) -> bool
-//{
-//    return a.first.second > b.first.second;
-//});
+    sort(tempVec.begin(), tempVec.end(),
+    [](const pair<pair<int, int>, unsigned short> & a, const pair<pair<int, int>, unsigned short> & b) -> bool
+{
+    return a.first.second > b.first.second;
+});
 
     for (int i = 0; i < tempVec.size(); i++) {
         //printf("INput: %p %p %p\n", tempVec[i].first.first, tempVec[i].first.second, tempVec[i].second);
