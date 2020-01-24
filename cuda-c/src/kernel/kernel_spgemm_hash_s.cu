9726d89c39a85106b1128bf33b9ec3bfb393af0c
DmiitriyJarosh <dimajarosh@gmail.com>
2020-01-24 19:47:30 +0300

    inline gr2

--- cuda-c/src/kernel/kernel_spgemm_hash_s.cu
+++ cuda-c/src/kernel/kernel_spgemm_hash_s.cu
@@ -653,8 +653,8 @@ __device__ real mult(real a, real b) {
 //            mult |= device_grammar_body[i];
 //        }
 //    }
-
     if ((0x20004 & conc) == 0x20004) {
+       printf("@@@\n");
         mult |= 0x1;
     } else if ((0x10008 & conc) == 0x10008) {
         mult |= 0x4;
